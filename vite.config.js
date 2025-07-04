dafdca470dd7aec6aed2001fb80c99176c0a876d
ALFIYA A <alfiyazon@gmail.com>
2025-07-04 13:34:38 +0530

    update

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/', // ✅ ensures correct paths for assets and styles on Vercel
 })
